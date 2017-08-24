82ce59cca189553265f25f7588f1403c8db290e9
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-08-24 07:59:17 -0400

    feat(TransactionContract):

---
+++ contracts/CoffeeTransaction.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.0.4;
+
+/**
+ * Sample coffee transaction
+ */
+contract CoffeeTransaction {
+
+  // Etherium Addresses
+  address[6] public buyers;
+
+  function buy(uint coffeeId) public returns (uint) {
+    // Similar to a `If` statement to check if 
+    require(coffeeId >= 0 && coffeeId <= 5);
+
+    //  To get the address of the person or smart contract who called this function, we use msg.sender.
+    buyers[coffeeId] = msg.sender;
+
+    // Confirmation
+    return coffeeId;
+  }
+
+  function getBuyers() public returns (address[6]) {
+    return buyers;
+  }
+}
