f4fbcf8819a5a0096c3645fd241127cf7fe2bbe0
Joshua John Villahermosa <joshvillahermosa@gmail.com>
2017-08-27 20:31:32 -0400

    WIP on modifying contacts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var CoffeeTransaction = artifacts.require('./CoffeeTransaction.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
